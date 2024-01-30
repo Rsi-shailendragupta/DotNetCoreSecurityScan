e80d00b2282197bd0a8d2d5f43d383ef5c563e31
Shailendra Gupta <Shailendra.Gupta@rsystems.com>
2024-01-30 16:31:44 +0530

    test commit

--- SecurityScansTestDOtNetCore/Program.cs
+++ SecurityScansTestDOtNetCore/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline skg.
+// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
